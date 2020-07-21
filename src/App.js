f0bc00fe8e16beb5f08d35cdc3af50dad9b5dc6d
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-20 17:54:09 -0700

    inserted navbar and links. links need styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
 import './App.css';
+
+import Games from './components/games'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/games" component={Games} />
+      </div>
+    </Router>
+
+
+
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <Link to='/games'>
+          <li>Games</li>
+        </Link>
+        <button>Sign Out</button>
+      </ul>
+    </nav>
   );
 }
 
---
+++ src/components/games.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Games() {
+    return (
+    <div>
+        Hello
+    </div>
+    );
+}
+
+export default Games;
