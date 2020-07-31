473933c158d7dcbe9a7b7d0a105a459c43da07d4
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-30 18:05:39 -0700

    global state buggy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -14,17 +14,23 @@ import DefaultPicGame from './components/default-pic'
 import SignUp from './components/sign-up'
 import SignIn from './components/sign-in'
 
+import {User} from './User'
+
 function App() {
+  const[value, setValue] = useState('hi');
+
   return (
     <Router>
       <div>
         <Navbar />
-        <Route path="/" exact component={Home} /> 
-        <Route path="/memory-squares" exact component={MemorySquares} />
-        <Route path="/picture-game" exact component={PicGame} />
-        <Route path="/default-pic" exact component={DefaultPicGame} />
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
+        <User.Provider value={{value,setValue}}>
+          <Route path="/" exact component={Home} /> 
+          <Route path="/memory-squares" exact component={MemorySquares} />
+          <Route path="/picture-game" exact component={PicGame} />
+          <Route path="/default-pic" exact component={DefaultPicGame} />
+          <Route path="/sign-up" exact component={SignUp} />
+          <Route path="/sign-in" exact component={SignIn} />
+        </User.Provider>
       </div>
     </Router>
   );
--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import fire from '../fire'
 
+import { User } from '../User'
+
 var u_name = "", p_name = "";
+
 function SignIn() {
-    // const 
+    const { value, setValue } = useContext(User);
 
     return (
         <div className="App-header">
             <h2>Sign In</h2>
             <div>
                 <label>Username: </label>
-                <input id='username' type='text' onChange={() => {setUname(document.getElementById('username').value)}} />
+                <input id='username' type='text' onChange={() => { setUname(document.getElementById('username').value) }} />
                 <br></br>
                 <label>Password: </label>
-                <input id='password' type='password' onChange={() => {setPname(document.getElementById('password').value)}} />
+                <input id='password' type='password' onChange={() => { setPname(document.getElementById('password').value) }} />
             </div>
+            <div>{value}</div>
             <br></br>
-            <button onClick={()=>{verify()}}>Enter</button>
+            <button onClick={() => {
+                var ref = fire.firestore().collection('users').doc(u_name);
+
+                ref.get().then(function (doc) {
+                    if (doc.exists) {
+                        if (doc.data()['password'] === p_name) {
+                            console.log('success!')
+                            setValue(u_name)
+                            console.log({ value }, u_name)
+                        } else {
+                            console.log('failure')
+                        }
+                    } else {
+                        alert("User does not exist");
+                    }
+                }).catch(function (error) {
+                    alert("System Error");
+                    console.log("Error getting document:", error);
+                });
+            }}>Enter</button>
         </div>
     );
 }
@@ -27,25 +50,6 @@ function setUname(input) {
 
 function setPname(input) {
     p_name = input;
-}
-
-function verify() {
-    var ref = fire.firestore().collection('users').doc(u_name);
-
-    ref.get().then(function(doc) {
-        if (doc.exists) {
-            if(doc.data()['password'] === p_name){
-                console.log('success!')
-            } else {
-                console.log('failure')
-            }
-        } else {
-            alert("User does not exist");
-        }
-    }).catch(function(error) {
-        alert("System Error");
-        console.log("Error getting document:", error);
-    });
 }
 
 export default SignIn;
