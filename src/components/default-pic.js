7e0c32de96aca8695d9d5d3cc9255be3042fbfa9
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-31 20:38:46 -0700

    buttons v1

--- src/components/default-pic.js
+++ src/components/default-pic.js
@@ -4,24 +4,68 @@ import Apple from '../apple.jpg'
 import Banana from '../banana.jpg'
 
 var x = [[Apple,'apple'],[Banana,'banana']];
-var i = 0;
+var i, score;
+const total = x.length;
 
 function DefaultPicGame(){
+    reset()
     return(
         <div className= "App-header">
             <h2>Picture Game</h2>
-            <img id = "1" src={x[i][0]} alt="Raiders" width="30%"/>
-            <input></input>
+            <img id = 'image' src={x[i][0]} alt="Raiders" width="30%"/>
+            <input/>
             <button onClick={() => {
-                i += 1;
-                document.getElementById('1').src = x[(i)%2][0];
+                if(i + 1 < total){
+                    i += 1;
+                    document.getElementById('image').src = x[i][0];
+                    if(i + 1 === total){
+                        console.log('yay')
+                    }
+                }
             }} >Check</button>
             <button onClick={() => {
-                i += 1;
-                document.getElementById('1').src = x[(i)%2][0];
+                if(i + 1 < total){
+                    i += 1;
+                    score += 1;
+                    document.getElementById('image').src = x[i][0];
+                    if(i + 1 === total){
+                        console.log('yay')
+                    }
+                }
             }} >Overwrite</button>
+            <button onClick={() => {
+                if(i + 1 < total){
+                    i += 1;
+                    document.getElementById('image').src = x[i][0];
+                    if(i + 1 === total){
+                        console.log('yay')
+                    }
+                }
+            }} >Skip</button>
         </div>
     )
+}
+
+function reset(){
+    i = 0;
+    score = 0;
+    var temp = [], location;
+
+    console.log('copy ',x)
+
+    for (var y = 0; y < total; y++) {
+        location = Math.floor(Math.random() * (total - y)); //random number in color array
+        temp.push(x[location])
+        x.splice(location,1)
+    }
+    x = temp;
+    console.log(temp)
+}
+
+function recordScore(user, real_score){
+    if(user !== ''){
+
+    }
 }
 
 export default DefaultPicGame;
