07648c17119f857b5999009a4c2a1e932790ff72
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-08 10:45:55 -0700

    buttons are in

--- src/components/CustomPicGame.js
+++ src/components/CustomPicGame.js
@@ -9,7 +9,22 @@ class CustomPicGame extends Component {
 
         this.state = {
             images : [],
-            length : 0
+            total : 0,
+            index : 0,
+            score : 0,
+            inputStyle : {
+                position: 'absolute',
+                bottom: '10px',
+                marginBottom: '50px',
+                float: 'right',
+                marginTop: 'auto',
+            },
+            bottomButtons : {
+                position: 'absolute',
+                bottom: '5px',
+                float: 'right',
+                marginTop: 'auto'
+            }
         }
     }
 
@@ -28,13 +43,34 @@ class CustomPicGame extends Component {
                         img.getDownloadURL().then(url => {
                             temp.push([img.name, url])
                             this.setState({
-                                images : temp
+                                images : temp,
+                                total : temp.length
                             })
                         })
                     })
                 }
             )
         }   
+    }
+
+    incrementIndex(old){
+        this.setState({
+            index : old + 1
+        })
+    }
+
+    incrementScore(old){
+        this.setState({
+            score : old + 1
+        })
+    }
+
+    recordScore(user, score){
+
+    }
+
+    reset(){
+
     }
 
     render(){
@@ -47,13 +83,63 @@ class CustomPicGame extends Component {
         } else if(this.state.images[0] === undefined){
             return (
                 <div className="App-general">
-                   <h2>{this.props.location.state['game']['game']}</h2>
+                   <h2>Loading...</h2>
                 </div>)
         }
         return (
             <div className="App-general">
                <h2>{this.props.location.state['game']['game']} lmao</h2>
-               <img width={"40%"} src={this.state.images[0][1]} alt="rip"></img>
+               <img width="30%" src={this.state.images[0][1]} alt="rip"></img>
+               <div style={this.state.inputStyle}>
+                <label>Image name: </label>
+                <input id='input' />
+                <button onClick={() => {
+                    if (this.state.index < this.state.total) {
+                        if (document.getElementById('input').value.toUpperCase() ===
+                        this.state.images[this.state.index][1].toUpperCase()) {
+                            this.incrementScore(this.state.score)
+                        }
+                        document.getElementById('input').value = ''
+                        this.incrementIndex(this.state.index);
+                        if (this.state.index < this.state.total) {
+                            document.getElementById('image').src = this.state.images[this.state.index][0];
+                        }
+                    }
+                    
+                    if (this.state.index === this.state.total) {
+                        this.recordScore(this.props.user, this.state.score)
+                        this.incrementIndex(this.state.index);
+                    }
+                }} >Check</button>
+            </div>
+            <div style={this.state.bottomButtons}>
+                <button onClick={() => {
+                    if (this.state.index < this.state.total) {
+                        this.incrementIndex(this.state.index);
+                        this.incrementScore(this.state.score)
+                        if (this.state.index < this.state.total) {
+                            document.getElementById('image').src = this.state.images[this.state.index][0];
+                        }
+                    }
+                    if (this.state.index === this.state.total) {
+                        this.recordScore(this.props.user, this.state.score)
+                        this.incrementIndex(this.state.index);
+                    }
+                }} >Overwrite</button>
+                <button onClick={() => {
+                    if (this.state.index < this.state.total) {
+                        this.incrementIndex(this.state.index);
+                        if (this.state.index < this.state.total) {
+                            document.getElementById('image').src = this.state.images[this.state.index][0];
+                        }
+                    }
+                    if (this.state.index === this.state.total) {
+                        this.recordScore(this.props.user, this.state.score)
+                        this.incrementIndex(this.state.index);
+                    }
+                }} >Skip</button>
+                <button onClick={() => this.reset()} >Start Over</button>
+            </div>
             </div>)
     }
 }
