6582554e1f9b47a715970bc0c4e1dd117fc710b7
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-12 11:26:46 -0700

    done for user not logged in

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+// hooks
+import {User} from '../User'
 
 function MyProfile(){
+    const user = useContext(User);
+
+    if(user.value === '' || user.value == null){
+        return (
+            <div className="App-general">
+                <p className="title">Log in to view your profile!</p>
+            </div>
+        )
+    }
+
     return (
         <div>
-            Test
+            {user.value}
         </div>
     )
 }
